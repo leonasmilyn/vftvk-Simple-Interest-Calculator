da871b3226047ddb7727d53f0bb6bdb067add4ba
leonasmilyn <85541003+leonasmilyn@users.noreply.github.com>
2021-06-23 14:28:54 +0400

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,7 @@
+function compute() {
+    var amount = document.getElementById("amount");
+    var interest = document.getElementById("interest");
+    var years = document.getElementById("years");
+    
+    result  amount * interest * years;
+}
